17c3b4afd0af8454df6ff07a154fdffac19399c4
Materka11 <arekmaterka11@gmail.com>
2025-04-19 10:28:32 +0200

    Feature: Add contacts to data

--- ContactsApp/Data/AppDbContext.cs
+++ ContactsApp/Data/AppDbContext.cs
@@ -6,5 +6,21 @@ namespace ContactsApp.Data
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //ustawienie unikalnego indeksu na polu Email w tabeli Users
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            //ustawienie unikalnego indeksu na polu Email w tabeli Conatcts
+            modelBuilder.Entity<Contact>()
+                .HasIndex(c => c.Email)
+                .IsUnique();
+        }
     }
 }
--- ContactsApp/obj/Debug/net9.0/ContactsApp.AssemblyInfo.cs
+++ ContactsApp/obj/Debug/net9.0/ContactsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4885c830f84d4a5d1fb2b9c5c5349e834e5a2f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7662bc6e88465d67ac3b3acbaef06922be253ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
